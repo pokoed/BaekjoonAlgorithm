8a156a790172e63dc76457aac26df732e37f4176
pokoed <20041230andy@gmail.com>
2021-03-25 13:46:20 +0900

    baekjoon11866 solved 210325

--- src/class2/baekjoon11866.java
+++ src/class2/baekjoon11866.java
@@ -1,5 +1,35 @@
 package class2;
 
-public class baekjoon11866 {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class baekjoon11866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('<');
+
+
+        while (q.size() >= 1) {
+            for (int i = 0; i < K - 1; i++) {
+                int val = q.poll();
+                q.offer(val);
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.deleteCharAt(sb.lastIndexOf(" "));
+        sb.append(">");
+        System.out.println(sb);
+    }
 }
